e1f4d63dd0e6b9bcf9ff39478b74266a63daa728
rvhack999 <rvHack@yandex.ru>
2025-05-21 03:17:13 +0800

    Initial commit

---
+++ app/src/main/java/com/autotrace/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.autotrace
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        imageView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            val imageUri: Uri? = data?.data
+            imageView.setImageURI(imageUri)
+        }
+    }
+}
